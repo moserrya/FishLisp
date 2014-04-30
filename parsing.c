17b81f614b3d1863e40542756bec9527eed113e4
Ryan Moser <ryanpmoser@gmail.com>
2014-04-29 22:30:19 -0700

    [UPDATE] parse user input

--- parsing.c
+++ parsing.c
@@ -48,7 +48,14 @@ int main(int argc, char** argv) {
 
     add_history(input);
 
-    printf("No, you're a %s\n", input);
+    mpc_result_t r;
+    if (mpc_parse("<stdin>", input, Lispy, &r)) {
+      mpc_ast_print(r.output);
+      mpc_ast_delete(r.output);
+    } else {
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
+    }
 
     free(input);
   }
