fdbe9ba0163f2dd3e03f7b668fc2041ba5ee0490
Ryan Moser <ryanpmoser@gmail.com>
2014-05-03 23:12:09 -0700

    [UPDATE] add pow

--- parsing.c
+++ parsing.c
@@ -29,24 +29,15 @@ typedef struct {
   int err;
 } lval;
 
-long min(long x, long y) {
-  if (x < y) { return x; }
-  return y;
-}
-
-long max(long x, long y) {
-  if (x > y) { return x; }
-  return y;
-}
-
 long eval_op(long x, char* op, long y) {
   if (strcmp(op, "+") == 0) { return x + y; }
   if (strcmp(op, "-") == 0) { return x - y; }
   if (strcmp(op, "*") == 0) { return x * y; }
   if (strcmp(op, "/") == 0) { return x / y; }
   if (strcmp(op, "%") == 0) { return x % y; }
-  if (strcmp(op, "min") == 0) { return min(x, y); }
-  if (strcmp(op, "max") == 0) { return max(x, y); }
+  if (strcmp(op, "min") == 0) { return fmin(x, y); }
+  if (strcmp(op, "max") == 0) { return fmax(x, y); }
+  if (strcmp(op, "pow") == 0) { return pow(x, y); }
   return 0;
 }
 
@@ -79,7 +70,7 @@ int main(int argc, char** argv) {
   mpca_lang(MPCA_LANG_DEFAULT,
     "                                                   \
     number   : /-?[0-9]+[.]?[0-9]*/ ;                   \
-    operator : '+' | '-' | '*' | '/' | '%' | \"min\" | \"max\" ;            \
+    operator : '+' | '-' | '*' | '/' | '%' | \"min\" | \"max\" | \"pow\" ; \
     expr     : <number> | '(' <operator> <expr>+ ')' ;  \
     lispy    : /^/ <operator> <expr>+ /$/ ;             \
     ",
