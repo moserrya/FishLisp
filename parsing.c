64aae3c9aeeafd1e02bd21847679770e6ed8550f
Ryan Moser <ryanpmoser@gmail.com>
2014-05-25 11:26:24 -0700

    add function to return string representations of enumerated types

--- parsing.c
+++ parsing.c
@@ -210,6 +210,18 @@ void lval_print(lval* v) {
 
 void lval_println(lval* v) { lval_print(v); putchar('\n'); }
 
+char* ltype_name(int t) {
+  switch(t) {
+    case LVAL_FUN: return "Function";
+    case LVAL_NUM: return "Number";
+    case LVAL_ERR: return "Error";
+    case LVAL_SYM: return "Symbol";
+    case LVAL_SEXPR: return "S-Expression";
+    case LVAL_QEXPR: return "Q-Expression";
+    default: return "Unknown";
+  }
+}
+
 struct lenv {
   int count;
   char** syms;
