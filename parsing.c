58ee2734af8700184e41a6baa5356aff2af2204d
Ryan Moser <ryanpmoser@gmail.com>
2014-06-07 22:21:51 -0700

    forward delcare parser pointers to use in loader

--- parsing.c
+++ parsing.c
@@ -25,6 +25,15 @@ void add_history(char* unused) {}
 
 /* Forward Declarations */
 
+mpc_parser_t* Number;
+mpc_parser_t* Symbol;
+mpc_parser_t* String;
+mpc_parser_t* Comment;
+mpc_parser_t* Sexpr;
+mpc_parser_t* Qexpr;
+mpc_parser_t* Expr;
+mpc_parser_t* Lispy;
+
 struct lval;
 struct lenv;
 typedef struct lval lval;
